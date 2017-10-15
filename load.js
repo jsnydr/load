6f936f6a973b9a1cb046dfa6ef6baf18ebe61127
jsnydr <jsnydr@jsnydr.com>
2017-10-15 01:48:04 -0500

    first-pass at streamlining
    the `module.exports` function can be used to wrap "local" code in a root project file or to "import" code from other project files
    
    wrapping behavior is invoked if it receives a function (just don't forget the snip-mark-comments) and import loading is invoked if it receives a string, interpretted as a path to a file expected to contain JavaScript or a directory potentially containing multiple ".js" files
    
    basic tests check out - could use to get those primitive tests committed here... and some README.md content

--- load.js
+++ load.js
@@ -1,26 +1,51 @@
 var fs = require('fs');
 var vm = require('vm');
 
-module.exports = function load(path, bootstrap){
+var load = function load(obj){
  if(!(tools.context)){
   tools.make_context(global);
  }
- if(fs.lstatSync(path).isDirectory()){
-  tools.dir(path);
+ if(typeof(obj) == 'function'){
+  // expect that function-type `obj` is a new load-context booting-up - we need to find and load it
+  tools.wrap_load();
  }else{
-  tools.file(path, bootstrap);
+  if(typeof(obj) == 'string'){
+   // expect that string-type `obj` is a path to a '.js' file or a directory with multiple
+   tools.load_path(obj);
+  }else{
+   throw new Error("not sure how to load the provided '" + typeof(obj) + "'");
+  }
  }
-}
+};
+
+module.exports = load;
 
 var tools = {
  context: null,
+ start_cut_after_this_line: "//->",
+ stop_cut_before_this_line: "//<-",
+
+ wrap_load: function(){
+  var filename = tools.filename_containing_caller(2);
+  var cut = tools.cut(fs.readFileSync(filename).toString());
+  tools.load_file(filename, {lineOffset: cut.skipped, script: cut.script});
+ },
+
+ load_path: function(path){
+  if(fs.lstatSync(path).isDirectory()){
+   tools.load_dir(path);
+  }else{
+   tools.load_file(path);
+  }
+ },
 
  make_context: function(where){
    tools.context = vm.createContext(where);
    tools.context.require = require;
+   tools.context.load = load;
  },
 
- file: function(full_path, bootstrap){
+ load_file: function(full_path, bootstrap){
   var options = {filename: full_path, displayErrors: true};
   var script = '';
   if(bootstrap){
@@ -32,11 +57,38 @@ var tools = {
   vm.runInContext(script, tools.context, options);
  },
 
- dir: function(dir_name){
+ load_dir: function(dir_name){
   fs.readdirSync(dir_name).forEach(function(file_name){
    if(file_name.slice(-3) == '.js'){
-    tools.file(dir_name + '/' + file_name);
+    tools.load_file(dir_name + '/' + file_name);
    }
   });
+ },
+
+ call_stack: function(){
+  var original_prepare_function = Error.prepareStackTrace;
+  Error.prepareStackTrace = function(_, stack_as_array){return stack_as_array;};
+  var err = new Error;
+  Error.captureStackTrace(err, arguments.callee);
+  var stack_trace = err.stack;
+  Error.prepareStackTrace = original_prepare_function;
+  return stack_trace;
+ },
+
+ filename_containing_caller: function(trace_back_levels){
+  if(!trace_back_levels){trace_back_levels=0;}
+  return tools.call_stack()[trace_back_levels+1].getFileName().toString();
+ },
+
+ cut: function(script){
+  var start_token = tools.start_cut_after_this_line;
+  var stop_token  = tools.stop_cut_before_this_line;
+  var start = script.indexOf(start_token) + start_token.length;
+  var stop  = script.lastIndexOf(stop_token);
+  start = script.indexOf("\n", start) + 1;
+  stop  = script.lastIndexOf("\n", stop);
+  skipped = script.slice(0,start).match(/\n/g).length
+  script = script.slice(start, stop);
+  return {skipped: skipped, script: script};
  }
-}
+};
