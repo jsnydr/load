5f31678560f41641c6f84cfb59b7f300fefab0d9
jsnydr <jsnydr@jsnydr.com>
2017-10-15 01:05:05 -0500

    failed to include in previous commit
    these changes to `load.js` took place, roughly, in parallel with the preparation of `boot.js` presented previously
    
    as before, I am still
    
    RECONSTRUCTING HISTORY

--- load.js
+++ load.js
@@ -1,34 +1,52 @@
 var fs = require('fs');
 var vm = require('vm');
 
-module.exports = function load(path){
- if(!(load_tools.context)){
-  load_tools.make_context(this);
+module.exports = function load(path, bootstrap){
+ if(!(tools.context)){
+  //load_tools.make_context(bootstrap ? bootstrap.where : this);
+  tools.make_context(global);
+  tools.context.require = require;
+//  tools.context.load = load;
+  //tools.context.process = process;
+  //tools.context.console = console;
+  //vm.runInContext("var require = this.constructor('return require')().exit()", tools.context);
  }
  if(fs.lstatSync(path).isDirectory()){
-  load_tools.dir(path);
+  tools.dir(path);
  }else{
-  load_tools.file(path);
+  tools.file(path, bootstrap);
  }
 }
 
-var load_tools = {
+var tools = {
  context: null,
 
  make_context: function(where){
-   this.context = where.__load_context = vm.createContext(where);
-   this.context.__load_context = this.context;
+   tools.context = vm.createContext(where);
+   // if the 'load' module's awareness of 'context' is not enough and the
+   //  calling context or the loaded context needs it, the following might
+   //  have some use...
+   //where.__load_context = this.context.__load_context = this.context;
  },
 
- file: function(full_path){
-  vm.runInContext(fs.readFileSync(full_path).toString(),
-                  this.context, {filename: full_path});
+ file: function(full_path, bootstrap){
+  var options = {filename: full_path, displayErrors: true};
+  var script = '';
+  if(bootstrap){
+   options.lineOffset = bootstrap.lineOffset;
+   script = bootstrap.script
+  }else{
+   script = fs.readFileSync(full_path).toString();
+  }
+  //runner = new vm.Script(script, options);
+  //runner.runInContext(tools.context, options);
+  vm.runInContext(script, tools.context, options);
  },
 
  dir: function(dir_name){
   fs.readdirSync(dir_name).forEach(function(file_name){
    if(file_name.slice(-3) == '.js'){
-    load_tools.file(dir_name + '/' + file_name);
+    tools.file(dir_name + '/' + file_name);
    }
   });
  }
