4c0f9c7afb8b0133d39d0e20a2330cf15d6361a3
jsnydr <jsnydr@jsnydr.com>
2017-10-15 01:09:07 -0500

    proof-of-concept cleaned-up and working
    swept away intermediate and partial forms and some notes to leave behind something that, more-or-less, does the job with the two part boot-root and load-branch system
    
    the `.runInContext` option for pushing a file name into stack-trace metadata does not seem to be working in my out-of-date node.js v0.10 install - a disappointment at this stage
    
    after all, useful stack-traces are most of the reason to do it this way instead of just `eval`ing strings read from files directly
    
    got a few ideas about how to streamline presentation and usage of the toolset - maybe I'll put some of that together and then reach-out for advice...

--- boot.js
+++ boot.js
@@ -1,16 +1,10 @@
 var fs = require('fs');
 var load = require('./load');
 
-// a hack: boot() will load its 'callback' ("post_boot_code_dont_run") by finding
-//  the file it was called from and loading those lines of javascript into a
-//  virtual machine context that the 'load' module controls
 module.exports = function boot(post_boot_code_dont_run){
- var line = 0; //tools.line(1);
  var file = tools.file(1);
- //console.log("file: '"+file+"'; line: '"+line.toString()+"'");
  var cut = tools.cut(fs.readFileSync(file).toString());
- //console.log(cut);
- load(file, {where: this, lineOffset: cut.skipped, script: cut.script});
+ load(file, {lineOffset: cut.skipped, script: cut.script});
 }
 
 var tools = {
@@ -27,13 +21,6 @@ var tools = {
   return stack_trace;
  },
 
-/*
- line: function(above){
-  if(!above){above=0;}
-  return tools.stack()[above+1].getLineNumber();
- },
-*/
-
  file: function(above){
   if(!above){above=0;}
   return tools.stack()[above+1].getFileName();
@@ -46,36 +33,8 @@ var tools = {
   var stop  = script.lastIndexOf(stop_token);
   start = script.indexOf("\n", start) + 1;
   stop  = script.lastIndexOf("\n", stop);
-  // will always have at least one skipped line... and advanced start past it
-  //skipped = (script.slice(0,start).match(/\n/g) || []).length + 1
   skipped = script.slice(0,start).match(/\n/g).length
   script = script.slice(start, stop);
   return {skipped: skipped, script: script};
- },
-/*
- nthIndexOf: function(searchIn, findPattern, n){
-  var length = searchIn.length;
-  var searchFrom = 0;
-  while((searchFrom = searchIn.indexOf(findPattern,searchFrom)) != -1){
-   if((--n)==0){
-    return searchFrom;
-   }else{
-    searchFrom++;
-   }
-  }
-  return -1;
  }
-*/
 }
-
-
-/*
-load.boot = function(where){
- path = boot_tools.file(1);
- skip_lines = boot_tools.line(1); 
-// had in mind to read the executing file, peel-off already executed lines, execute the remainder, then wait (efficiently) for exit condition
-// more complicated than it is worth: some complexity to execute remainder of file but almost hopeless to wait efficiently in unknown
-// version of node (later versions have "child_process.execSync('sleep 10')" which could be "while(1)" looped)
-  vm.runInContext(fs.readFileSync(full_path).toString(), load_tools.context(where), {filename: full_path});
-}
-*/
--- load.js
+++ load.js
@@ -3,13 +3,7 @@ var vm = require('vm');
 
 module.exports = function load(path, bootstrap){
  if(!(tools.context)){
-  //load_tools.make_context(bootstrap ? bootstrap.where : this);
   tools.make_context(global);
-  tools.context.require = require;
-//  tools.context.load = load;
-  //tools.context.process = process;
-  //tools.context.console = console;
-  //vm.runInContext("var require = this.constructor('return require')().exit()", tools.context);
  }
  if(fs.lstatSync(path).isDirectory()){
   tools.dir(path);
@@ -23,10 +17,7 @@ var tools = {
 
  make_context: function(where){
    tools.context = vm.createContext(where);
-   // if the 'load' module's awareness of 'context' is not enough and the
-   //  calling context or the loaded context needs it, the following might
-   //  have some use...
-   //where.__load_context = this.context.__load_context = this.context;
+   tools.context.require = require;
  },
 
  file: function(full_path, bootstrap){
@@ -38,8 +29,6 @@ var tools = {
   }else{
    script = fs.readFileSync(full_path).toString();
   }
-  //runner = new vm.Script(script, options);
-  //runner.runInContext(tools.context, options);
   vm.runInContext(script, tools.context, options);
  },
 
